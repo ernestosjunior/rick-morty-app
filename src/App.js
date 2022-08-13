c54dcdbe58511baddd093cf752d6fa0ed6ec691b
Ernesto Junior <ernesto.sjunior@hotmail.com>
2022-08-13 20:05:27 -0300

    feat: init create header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import { useQuery, gql } from "@apollo/client";
 
 const GET_LOCATIONS = gql`
@@ -13,24 +12,7 @@ const GET_LOCATIONS = gql`
 function App() {
   const { loading, error, data } = useQuery(GET_LOCATIONS);
   console.log(loading, error, data);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import { ReactComponent as Logo } from "../../assets/svgs/logo.svg";
+
+export const Header = () => {
+  return (
+    <>
+      <Logo />
+    </>
+  );
+};
