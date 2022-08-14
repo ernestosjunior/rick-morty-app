58a06fdc38f18b293aa3dd11f2c8323564979a63
Ernesto Junior <ernesto.sjunior@hotmail.com>
2022-08-13 21:55:38 -0300

    feat: passed props to header on baseLayout

--- src/containers/BaseLayot/BaseLayout.jsx
+++ src/containers/BaseLayot/BaseLayout.jsx
@@ -1,9 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import { Header } from "../../components";
 
 export const BaseLayout = ({ children }) => {
+  const navigate = useNavigate();
   return (
     <>
-      <Header />
+      <Header
+        redirectToHome={() => navigate("/")}
+        redirectToList={() => navigate("/favorites")}
+      />
       {children}
     </>
   );
