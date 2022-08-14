0355ab540ec960f8be64896d5e769ebe53082f23
Ernesto Junior <ernesto.sjunior@hotmail.com>
2022-08-13 22:07:32 -0300

    feat: config uselocalstorage

--- src/store/root/provider.jsx
+++ src/store/root/provider.jsx
@@ -1,12 +1,24 @@
 import { useReducer } from "react";
+import { useLocalStorage } from "react-use";
 import { RootContext } from "./context";
 import { initialState, rootReducer } from "./reducer";
 
 export const RootProvider = ({ children }) => {
   const [rootState, rootDispatch] = useReducer(rootReducer, initialState);
+  const [token, setToken, removeToken] = useLocalStorage("token", "");
+  const isLogged = !!token;
 
   return (
-    <RootContext.Provider value={{ rootState, rootDispatch }}>
+    <RootContext.Provider
+      value={{
+        rootState,
+        rootDispatch,
+        token,
+        setToken,
+        removeToken,
+        isLogged,
+      }}
+    >
       {children}
     </RootContext.Provider>
   );
