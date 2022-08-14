e90a6bbb1a5dfb56d73c69f592ab0b980137ce1a
Ernesto Junior <ernesto.sjunior@hotmail.com>
2022-08-13 21:53:19 -0300

    feat: create header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,49 @@
+import classnames from "classnames";
 import { ReactComponent as Logo } from "../../assets/svgs/logo.svg";
 
-export const Header = () => {
+export const Header = ({
+  isLogged,
+  username,
+  redirectToList,
+  redirectToHome,
+}) => {
+  const label = isLogged ? "LOGOUT" : "LOGIN";
+
   return (
-    <>
-      <Logo />
-    </>
+    <header className="flex justify-between items-center pr-6 pl-6 h-60">
+      <Logo className="w-40" />
+      <div className="flex items-center gap-7">
+        <p
+          className="font-bold text-lg text-dark-color cursor-pointer"
+          onClick={redirectToHome}
+        >
+          Home
+        </p>
+        {isLogged && (
+          <>
+            <p
+              className="font-bold text-lg text-orange-color cursor-pointer"
+              onClick={redirectToList}
+            >
+              Favorites List
+            </p>
+            <p className="font-bold text-dark-color text-lg">Hi, {username}</p>
+          </>
+        )}
+        <button
+          className={classnames(
+            "border border-solid rounded-lg pt-2 pb-2 pl-4 pr-4 font-bold text-dark-color hover:text-white",
+            {
+              "border-[#fb4e5f]": isLogged,
+              "hover:bg-[#fb4e5f]": isLogged,
+              "border-[#00c8be]": !isLogged,
+              "hover:bg-[#00c8be]": !isLogged,
+            }
+          )}
+        >
+          {label}
+        </button>
+      </div>
+    </header>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        40: "40px",
+      },
+      height: {
+        60: "60px",
+      },
+      colors: {
+        "red-color": "#fb4e5f",
+        "dark-color": "rgb(51, 51, 51)",
+        "orange-color": "rgb(255, 152, 0)",
+      },
+    },
   },
   plugins: [],
 };
